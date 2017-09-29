3896cddaeb148d2a8bfbcf61669d68b9df49e43b
Catherine Rice <clr2107@gmail.com>
2017-09-29 15:49:30 -0400

    Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,31 @@ users = 100.times.map do
 					:password => 'password')
 end
 
-questions = 20.times.map do
-	Questions.create( :user_id => Faker::Number(10),
-					:question => Faker::Name.RickAndMorty.quote)
+Question.create [{ :user_id => 1},
+								 { :user_id => 2},
+								 { :user_id => 3},
+								 { :user_id => 4}]
+
 end
 
 answers = 20.times.map do
-	Answers.create( :question_id => Faker::Number(10),
-					:user_id =>Faker::Number(10),
+	Answer.create( :question_id => Faker::Number.number(10),
+					:user_id =>Faker::Number.number(10),
 					:answer => Faker::HowIMetYourMother.quote)
+end
 
-comments = 20.times.map do
-	Comments.create( :comment => Faker::LordOfTheRings.location,
-					:question_id => Faker::Number(10),
-					:answer_id => Faker::Number(10),
-					:user_id => Faker::Number(10)
+comments = 10.times.map do
+	Comment.create( :comment_text => Faker::LordOfTheRings.location,
+					:commentable_type => "Question",
+					:commentable_id => Faker::Number.number(10),
+					:user_id => Faker::Number.number(10)
 					)
+end
+
+comments = 10.times.map do
+	Comment.create( :comment_text => Faker::LordOfTheRings.location,
+					:commentable_type => "Answer",
+					:commentable_id => Faker::Number.number(10),
+					:user_id => Faker::Number.number(10)
+					)
+end
